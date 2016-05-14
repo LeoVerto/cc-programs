61c57a35a19677e4edf55faa7d6175c04289604a
LeoVerto <LeoVerto@users.noreply.github.com>
2016-05-14 16:48:14 +0200

    Add onturner

---
+++ onturner.lua
@@ -0,0 +1,36 @@
+local computers = {}
+local configPath = "computers"
+local modemSide = "left"
+
+function loadConfig()
+  if fs.exists(configPath)  then
+    if fs.getSize(configPath) ~= 0 then
+      for line in io.lines(configPath) do
+        computers[line] = true
+      end
+      return true
+    else
+      print("Computer list empty")
+    end
+  else
+    print("Couldn't find computer list, generating empty file \""..configPath.."\"")
+    f = fs.open(configPath, "w")
+    f.flush()
+    f.close()
+  end
+  return false
+end
+
+function startComputers()
+  m = peripheral.wrap(modemSide)
+
+  for id, _ in pairs(computers) do
+    m.callRemote("computer_"..id, "turnOn")
+  end
+end
+
+if loadConfig() then
+  startComputers()
+end
+
+print(textutils.serialize(computers))
